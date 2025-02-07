7352e552c2dd3b461c48c936b27031b8288848c8
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2025-02-08 02:28:05 +0600

    dynamic query function done in blog-data.js file

--- README.md
+++ README.md
@@ -6,7 +6,77 @@
 - "lib" folder er modde graphQl er clien rakbo; er jonno file create korbo `graphQLClient.js`;
 
 
-# `graphQLClient.js`
+# 1. `graphQLClient.js`
 - ai file e amra age client ta setup korbo; er jonno client hisabe hashnode use korbo; `https://gql.hashnode.com`; amra ai link er browse korei GraphQL practise korsilam, ekhon ai link tai amader nextjs e client hisabe use korbo;
 
+## all post er jonno ai `gql` request likte hobe
+```js
+query postDetails{
+  publication(host:"blog.greenroots.info") {
+    title
+    posts (first:20){
+      pageInfo{
+        hasNextPage
+        endCursor
+      }
+      edges{
+        node{
+          author{
+            profilePicture
+            name
+          }
+          title
+          subtitle
+          brief
+          slug 
+          coverImage{
+            url
+          }
+          tags{
+            name
+            slug
+            id
+          }
+          publishedAt
+          readTimeInMinutes
+        }
+      }
+    }
+  }
+}
+```
 
+# 2. `blog-data.js` 
+- aikhane amra GraphQL use kore tapas da er data get korbo; ar `tags` ta jehoto dynamic use korbo tai er jonno amader onek kaj korte hoise just sodo dynamic korar jonno;
+
+
+## single post er jonno ai `gql` request likte hobe
+
+```js
+query postDetails{
+  publication(host:"blog.greenroots.info") {
+    post (slug:"exploring-advanced-javascript-features-ecmascript-2023"){
+      author{
+        name
+        profilePicture
+      }
+      publishedAt
+      title
+      subtitle
+      readTimeInMinutes
+      content{
+        html
+      }
+      tags{
+        name
+        slug
+        id
+      }
+      coverImage{
+        url
+      }
+    }
+  }
+    
+}
+```
---
+++ queries/blog-data.js
@@ -0,0 +1,82 @@
+import { getClient } from "@/lib/graphQLClient";
+import { gql } from "graphql-request";
+
+export const getAllPosts = async (tags) => {
+  const client = getClient();
+
+  const data = await client.request(
+    gql`
+      query allPosts($tag: [ObjectId!]) {
+        publication(host: "blog.greenroots.info") {
+          title
+          posts(first: 20, filter: { tags: $tags }) {
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+            edges {
+              node {
+                author {
+                  profilePicture
+                  name
+                }
+                title
+                subtitle
+                brief
+                slug
+                coverImage {
+                  url
+                }
+                tags {
+                  name
+                  slug
+                  id
+                }
+                publishedAt
+                readTimeInMinutes
+              }
+            }
+          }
+        }
+      }
+    `,
+    { tags: tags }
+  );
+
+  return data.publication.posts.edges;
+};
+
+export const getPost = async (slug) => {
+  const client = getClient();
+
+  const data = await client.request(
+    gql`
+      query postDetails($slug: String!) {
+        publication(host: "blog.greenroots.info") {
+          post(slug: $slug) {
+            author {
+              name
+              profilePicture
+            }
+            publishedAt
+            title
+            subtitle
+            readTimeInMinutes
+            content {
+              html
+            }
+            tags {
+              name
+              slug
+              id
+            }
+            coverImage {
+              url
+            }
+          }
+        }
+      }
+    `,
+    { slug: slug }
+  );
+};
